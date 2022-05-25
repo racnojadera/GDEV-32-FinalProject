7c9729acfffbb2a7cc2432e726a3ac272b4fa982
clarkpogi <clark.paguio@gmail.com>
2022-05-25 21:51:19 +0800

    Fixing skybox stutter bug

--- Main.cpp
+++ Main.cpp
@@ -166,34 +166,28 @@ int main()
 	float skyboxVertices[] =
 	{
 		//   Coordinates
-		-1.0f, -1.0f,  1.0f,//        7--------6
-		 1.0f, -1.0f,  1.0f,//       /|       /|
-		 1.0f, -1.0f, -1.0f,//      4--------5 |
-		-1.0f, -1.0f, -1.0f,//      | |      | |
-		-1.0f,  1.0f,  1.0f,//      | 3------|-2
-		 1.0f,  1.0f,  1.0f,//      |/       |/
-		 1.0f,  1.0f, -1.0f,//      0--------1
+		-1.0f, -1.0f,  1.0f,
+		 1.0f, -1.0f,  1.0f,
+		 1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f,  1.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,
+		 1.0f,  1.0f, -1.0f,
 		-1.0f,  1.0f, -1.0f
 	};
 
 	unsigned int skyboxIndices[] =
 	{
-		// Right
 		1, 2, 6,
 		6, 5, 1,
-		// Left
 		0, 4, 7,
 		7, 3, 0,
-		// Top
 		4, 5, 6,
 		6, 7, 4,
-		// Bottom
 		0, 3, 2,
 		2, 1, 0,
-		// Back
 		0, 1, 5,
 		5, 4, 0,
-		// Front
 		3, 7, 6,
 		6, 2, 3
 	};
@@ -226,18 +220,15 @@ int main()
 		"back.jpg"
 	};
 
-	// Creates the cubemap texture object
 	unsigned int cubemapTexture;
 	glGenTextures(1, &cubemapTexture);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	// These are very important to prevent seams
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	// This might help with seams on some systems
-	//glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
 	for (unsigned int i = 0; i < 6; i++)
 	{
@@ -297,9 +288,6 @@ int main()
 	glBindVertexArray(0);
 	GLuint program = CreateShaderProgram("main.vsh", "main.fsh");
 	
-
-	//skyboxShader.Activate();
-	// glUniform1i(glGetUniformLocation(skyboxShader.ID, "skybox"), 0);
 
 	GLuint depthVAO;
 	glGenVertexArrays(1, &depthVAO);
@@ -375,8 +363,6 @@ int main()
 		glm::mat4 pyramid1 = glm::mat4(1.0f);
 		glm::mat4 pyramid2 = glm::mat4(1.0f);
 		glm::mat4 pyramid3 = glm::mat4(1.0f);
-
-
 
 		pyramid1 = glm::translate(pyramid1, glm::vec3(-2.0f, 1.5f, 3.0f));
 		pyramid1 = glm::rotate(pyramid1, glm::radians(25.0f*time), glm::vec3(0.0f, 1.0f, 0.0f));
@@ -455,24 +441,20 @@ int main()
 
 		//skybox
 		glUseProgram(skyboxShader);
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
-		// We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
-		// The last row and column affect the translation of the skybox (which we don't want to affect)
-		view = glm::mat4(glm::mat3(glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp)));
-		projection = glm::perspective(glm::radians(45.0f), (float)windowWidth / windowHeight, 0.1f, 100.0f);
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(glm::mat3(glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp)));
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 100.0f);
+
+		glUniformMatrix4fv(glGetUniformLocation(skyboxShader, "model"), 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(glGetUniformLocation(skyboxShader, "view"), 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(glGetUniformLocation(skyboxShader, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
-		// Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
-		// where an object is present (a depth of 1.0f will always fail against any object's depth value)
 		glBindVertexArray(skyboxVAO);
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
 		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
-		// Switch back to the normal depth function
 		glDepthFunc(GL_LESS);
 
 		glBindVertexArray(0);
--- skybox.vsh
+++ skybox.vsh
@@ -5,12 +5,11 @@ out vec3 texCoords;
 
 uniform mat4 projection;
 uniform mat4 view;
+uniform mat4 model;
 
 void main()
 {
-    vec4 pos = projection * view * vec4(aPos, 1.0f);
-    // Having z equal w will always result in a depth of 1.0f
-    gl_Position = vec4(pos.x, pos.y, pos.w, pos.w);
-    // We want to flip the z axis due to the different coordinate systems (left hand vs right hand)
-    texCoords = vec3(aPos.x, aPos.y, -aPos.z);
+	texCoords = vec3(model * vec4(aPos, 1.0));
+    vec4 pos = projection * view * model * vec4(aPos, 1.0f);
+    gl_Position = vec4(pos.x, pos.y, pos.w, pos.w);    
 }    
